01d2204696a9b363733f3c1972f87ebb60178e8c
2023361366 <2023361366@qq.com>
2016-10-14 13:50:37 +0800

    第1次提交

---
+++ app/src/main/java/com/example/lian/xiangmu_kuangjia/SystemSet.java
@@ -0,0 +1,13 @@
+package com.example.lian.xiangmu_kuangjia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SystemSet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_set);
+    }
+}
